5a3b0597d5630ba49358a87dd7d6b0a989ba78ea
Xenthio <thegooster88@gmail.com>
2025-11-06 15:34:56 +1100

    Initial commit

---
+++ OldiOSExperience/Data/AppInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace OldiOSExperience.Data // Make sure this namespace matches your project
+{
+    public class AppInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IconPath { get; set; }
+
+        // We can add more later, like which component to launch
+        // public Type ComponentToLaunch { get; set; }
+    }
+}
