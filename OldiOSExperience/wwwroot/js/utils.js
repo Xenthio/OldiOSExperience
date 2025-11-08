7099972b7951df3fe12c7fbc9f30d2b288e1355e
Xenthio <thegooster88@gmail.com>
2025-11-08 20:47:13 +1100

    scaling system changes

--- OldiOSExperience/wwwroot/js/utils.js
+++ OldiOSExperience/wwwroot/js/utils.js
@@ -4,3 +4,30 @@ function getElementRect(element) {
     }
     return element.getBoundingClientRect();
 }
+function getElementHeight(element) {
+    if (!element) {
+        return 0;
+    }
+    return element.getBoundingClientRect().height;
+}
+
+// Keep track of the Blazor component reference for the resize event
+let blazorResizeRef = null;
+
+// The function that gets called on window resize
+const resizeHandler = () => {
+    if (blazorResizeRef) {
+        blazorResizeRef.invokeMethodAsync('OnBrowserResize');
+    }
+};
+
+// Functions to add and remove the listener
+function addResizeListener(dotNetObjectRef) {
+    blazorResizeRef = dotNetObjectRef;
+    window.addEventListener('resize', resizeHandler);
+}
+
+function removeResizeListener(dotNetObjectRef) {
+    window.removeEventListener('resize', resizeHandler);
+    blazorResizeRef = null;
+}
